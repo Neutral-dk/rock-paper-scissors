fb56cc12d88a4022cfa1acd89245a50a82c4256c
Neutral-DK <themallee@gmail.com>
2025-08-25 21:09:27 +0200

    Add scorecounter to playRound function

--- java.js
+++ java.js
@@ -21,17 +21,17 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     
         if (humanChoice === "Paper" && computerChoice === "Scissors") {
-            return console.log("You loose! Scissors beats Paper");
+            return console.log("You loose! Scissors beats Paper") + computerScore++;
         } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-            return console.log("You win! Paper beats Rock"); 
+            return console.log("You win! Paper beats Rock") + humanScore++;
         } else if (humanChoice === "Scissors" && computerChoice === "Rock") { 
-            return console.log("You loose! Rock beats your Scissors");
+            return console.log("You loose! Rock beats your Scissors") + computerScore++;
         } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-            return console.log("You win! Scissors beat Paper"); 
+            return console.log("You win! Scissors beat Paper") + humanScore++;
         } else if (humanChoice === "Rock" && computerChoice === "Paper") {
-            return console.log("You loose! Paper beats rock");
+            return console.log("You loose! Paper beats rock") + computerScore++;
         } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-            return console.log("You win! Rock beats Scissors");
+            return console.log("You win! Rock beats Scissors") + humanScore++;
         } else { 
             return console.log("That's a tie!");
         }
@@ -42,3 +42,6 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+console.log(humanScore);
+console.log(computerScore);
